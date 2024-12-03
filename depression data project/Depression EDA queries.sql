810b3a5e326deda393e4ab75096d089416c44286
Prex12 <allihuzzain@gmail.com>
2024-12-03 23:32:49 +0100

    completed EDA and visualizations for projects

--- depression data project/Depression EDA queries.sql
+++ depression data project/Depression EDA queries.sql
@@ -2,6 +2,16 @@ SELECT Gender, COUNT(Gender), Depression
 FROM [Depression Student Dataset]
 GROUP BY Depression, Gender
 
+-- Average ages of people in the dataset is 26
+SELECT AVG(Age) average_age
+FROM [Depression Student Dataset]
+
+-- Age distrubution
+SELECT Age, COUNT(Age) number_in_group
+FROM [Depression Student Dataset]
+GROUP BY Age
+ORDER BY Age
+
 -- correlating study satisfaction and study hours to depression
 SELECT Depression, AVG(Study_Hours) AS Avg_Study_Hours 
 FROM [Depression Student Dataset]
@@ -30,17 +40,50 @@ ALTER TABLE [Depression Student Dataset]
 ALTER COLUMN Sleep_Duration int
 
 -- Relationship between Suicidal thoughts and study satisfaction
-SELECT Sleep_Duration, Study_Satisfaction, 
+SELECT Sleep_Duration, Study_Satisfaction, COUNT(Sleep_Duration),
        AVG(CASE 
-		WHEN Have_you_ever_had_suicidal_thoughts = 'Yes' THEN 1 
-		ELSE 0 END
+			WHEN Have_you_ever_had_suicidal_thoughts = 'Yes' THEN 1 
+			ELSE 0
+		END
 		) AS Suicidal_Thoughts_Rate 
+FROM [Depression Student Dataset]
+GROUP BY Sleep_Duration, Study_Satisfaction, Have_you_ever_had_suicidal_thoughts
+HAVING Have_you_ever_had_suicidal_thoughts = 'Yes'
+
+
+-- High academic pressure relatioinship to study satisfaction
+SELECT Academic_Pressure, AVG(Study_Satisfaction) average_study_satisfaction
+FROM [Depression Student Dataset]
+WHERE Academic_Pressure >= 4
+GROUP BY Academic_Pressure;
+
+-- Financial stress and its relationship with depression
+SELECT Financial_Stress, COUNT(*) AS Count, 
+       SUM(CASE WHEN Depression = 'Yes' THEN 1 ELSE 0 END) AS Depressed_Count 
 FROM [Depression Student Dataset] 
-GROUP BY Sleep_Duration, Study_Satisfaction;
+GROUP BY Financial_Stress;
 
 
+-- Relationship btw dietary habits and sleep hours more or less the same accross the board
+SELECT Dietary_Habits, AVG(Sleep_Duration) Average_sleep
+FROM [Depression Student Dataset]
+GROUP BY Dietary_Habits
 
+-- Sleep and study satisfation
+SELECT Sleep_Duration, AVG(Study_Satisfaction) avg_satisfaction
+FROM [Depression Student Dataset]
+GROUP BY Sleep_Duration
+
+-- avg study hours of individuals with high study satisfaction = 6 hours
+SELECT AVG(Study_Hours) avg_hours, Study_Satisfaction
+FROM [Depression Student Dataset]
+WHERE Study_Satisfaction >= 4
+GROUP BY Study_Satisfaction
+
+SELECT Family_History_of_Mental_Illness, Depression
+FROM [Depression Student Dataset]
+WHERE Family_History_of_Mental_Illness = 'Yes';
 
 
 SELECT *
-FROM [Depression Student Dataset]
+FROM [Depression Student Dataset];
